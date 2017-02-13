4da71a43b22d2e416603de914e1a8409f08afbfc
MeganMFisher <meganmichelle318@gmail.com>
2017-02-13 13:31:50 -0700

    completed

---
+++ js/products/productsCtrl.js
@@ -0,0 +1,8 @@
+angular.module('app').controller('productsCtrl', function($scope, $stateParams, productsService){
+    if($stateParams.id === 'shoes') {
+        $scope.productData = productsService.shoeData;
+    }
+    else if ($stateParams.id === 'socks') {
+        $scope.productData = productsService.sockData;
+    }
+})
